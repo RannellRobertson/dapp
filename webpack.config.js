f7d4cb8dc9c42b38ec43e326572d021881f0c7f8
Rannell <rrrprobertson@gmail.com>
2021-02-25 08:41:22 -0700

    social music dapp

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,39 @@
-const html = require('html-webpack-plugin')
-const path = require('path')
+import 'babel-polyfill'
+import html from 'html-webpack-plugin'
+import { join } from 'path'
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+export const entry = ['babel-polyfill', './src/index.js']
+export const output = {
+    filename: 'bundle.js',
+    path: join(__dirname, 'dist')
+}
+export const module = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
             }
-        ]
-    },
-    plugins: [
-        new html({
-            template: './src/index.html',
-            filename: './index.html'
-        })
+        }, {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'html-loader'
+            }
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' }
+            ]
+        }
     ]
 }
+export const plugins = [
+    new html({
+        template: './src/index.html',
+        filename: './index.html',
+    })
+]
